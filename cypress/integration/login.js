fe70ec004c6d308f5c585da69beac203cf5cc43d
MarkoDV <macanes02@gmail.com>
2020-07-26 22:54:06 +0200

    3rd commit

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,2 +1,49 @@
+/// <reference types="Cypress" />
+const faker = require('faker');
+let randomEmail = faker.internet.email();
+let randomPassword = faker.internet.password();
 
+describe('The Home Page', ()=> {
+    it('GA-28 Login-valid data', ()=> {
+      cy.visit('/');
+      cy.get('.nav-link').contains('Login').click();
+      cy.get('#email').type('test@test.com');
+      cy.get('#password').type('12345678');
+      cy.get('[type=submit]').click()
+      cy.wait(4000);
+      cy.get('.nav-link').contains('Logout').should('be.visible');
+    });
+    it.only('GA-22 Login- invalid data-email', ()=>{
+      cy.visit('/');
+      cy.get('.nav-link').contains('Login').click();
+      cy.get('#email').type(randomEmail);
+      cy.get('#password').type('12345678');
+      cy.get('[type=submit]').click();
+    });
+    it.only('GA-25 Login invalid data - password', ()=>{
+      cy.visit('/');
+      cy.get('.nav-link').contains('Login').click();
+      cy.get('#email').type('test@test.com');
+      cy.get('#password').type(randomPassword);
+      cy.get('[type=submit]').click();
+    });
+  });
+describe.only('Logged', ()=>{
+  it('GA-32 : User is logged', ()=> {
+    cy.visit('/')
+    cy.get('.nav-link').contains('Login').click()
+    cy.get('#email').type('jelllenakrstic@gmail.com')
+    cy.get('#password').type('jelenak1908')
+    cy.get('[type=submit]').click()
+    cy.wait(1000)
+    cy.get('.nav-link').contains('Logout').should('be.visible')
+    cy.get('.nav-link').contains('My Galleries')
+    cy.get('.nav-link').contains('Create Gallery')
+    cy.get('.nav-link').contains('Logout')
+    cy.get('.title-style').contains('All Galleries')
+    cy.wait(3000);
+    cy.get('.form-control').should('have.attr', 'placeholder', 'Search...');
+    cy.get('[type=button]').should('be.visible');
+  });
+});
 
---
+++ cypress/integration/register.js
@@ -0,0 +1,83 @@
+const faker = require('faker');
+let osnovnoIme = faker.name.findName();
+let prezIme = faker.name.lastName();
+let randomEmail = faker.internet.email();
+let randomPassword = faker.internet.password();
+
+
+describe('Registration page', ()=> {
+  beforeEach('Should be first page before registration', ()=>{
+    cy.visit('/register');
+  })
+    it('GA-14 : Register page positive test - valid data', ()=> {
+      cy.get('#first-name').type(osnovnoIme);
+      cy.get('#last-name').type(prezIme);
+      cy.get('#email').type(randomEmail);
+      cy.get('#password').type(randomPassword);
+      cy.get('#password-confirmation').type(randomPassword);
+      cy.get('input[type=checkbox]').check();
+      cy.get('button[type=submit]').click();
+      cy.wait(2000);
+      cy.location().should((loc)=>{
+         expect(loc.origin).to.eq('https://gallery-app.vivifyideas.com')
+      });
+    });
+    it('GA-81 : Confirmation password doesnt match', ()=>{
+      cy.get('#first-name').type(osnovnoIme);
+      cy.get('#last-name').type(prezIme);
+      cy.get('#email').type(randomEmail);
+      cy.get('#password').type(randomPassword);
+      cy.get('#password-confirmation').type('12345678');
+      cy.get('input[type=checkbox]').check();
+      cy.get('button[type=submit]').click();
+      cy.get('.alert').should('have.text', 'The password confirmation does not match.')
+    });
+    it('GA-40 : Register page test - First name input field: required', ()=>{
+      cy.get('#last-name').type(prezIme);
+      cy.get('#email').type(randomEmail);
+      cy.get('#password').type(randomPassword);
+      cy.get('#password-confirmation').type(randomPassword);
+      cy.get('input[type=checkbox]').check();
+      cy.get('button[type=submit]').click();
+      const getAlert = () =>{
+        return 'Please fill in this field'
+      };
+      cy.wrap({alert:getAlert}).invoke('alert').should('eq', 'Please fill in this field');
+    });
+    it('GA-46 : Register page test - Last name input field: required', ()=>{
+      cy.get('#first-name').type(osnovnoIme);
+      cy.get('#email').type(randomEmail);
+      cy.get('#password').type(randomPassword);
+      cy.get('#password-confirmation').type(randomPassword);
+      cy.get('input[type=checkbox]').check();
+      cy.get('button[type=submit]').click();
+    });
+    it('GA-59 : Register page test - Password input field empty', ()=> {
+      cy.get('#first-name').type(osnovnoIme);
+      cy.get('#last-name').type(prezIme);
+      cy.get('#email').type(randomEmail); 
+      cy.get('#password-confirmation').type(randomPassword);
+      cy.get('[type="checkbox"]').check();
+      cy.get('[type=submit]').click();
+    });
+    it('GA-83 : Password form - password has less then 8 characters', ()=>{
+      cy.get('#first-name').type(osnovnoIme);
+      cy.get('#last-name').type(prezIme);
+      cy.get('#email').type(randomEmail);
+      cy.get('#password').type('123');
+      cy.get('#password-confirmation').type('123');
+      cy.get('input[type=checkbox]').check();
+      cy.get('button[type=submit]').click();
+      cy.get('.alert').should('have.text', 'The password must be at least 8 characters.')
+    });
+    it.only('GA-84 : User cant register twice', ()=>{
+      cy.get('#first-name').type('test');
+      cy.get('#last-name').type('test');
+      cy.get('#email').type('ruzictam@gmail.com');
+      cy.get('#password').type('testtest2');
+      cy.get('#password-confirmation').type('testtest2');
+      cy.get('input[type=checkbox]').check();
+      cy.get('button[type=submit]').click();
+      cy.get('.alert').should('have.text', 'The email has already been taken.')
+    });
+});
