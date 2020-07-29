e77bbf29aa3622ca159cbb30c57bf88cd835ecf9
MarkoDV <macanes02@gmail.com>
2020-07-29 14:30:08 +0200

    Cy commit2

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -11,7 +11,7 @@ describe('The Home Page', ()=> {
     cy.server();
     cy.route(Cypress.env('apiUrl') + '/galleries?page=1&term=').as('galleries');
   });
-    it.only('GA-28 Login-valid data', ()=> {
+    it('GA-28 Login-valid data', ()=> {
       cy.get('.nav-link').contains('Login').click();
       authPage.email().type(user.email);
       authPage.password().type(user.password);
@@ -21,31 +21,32 @@ describe('The Home Page', ()=> {
     });
     it('GA-22 Login- invalid data-email', ()=>{
       cy.get('.nav-link').contains('Login').click();
-      cy.get('#email').type(randomEmail);
-      cy.get('#password').type('12345678');
-      cy.get('[type=submit]').click();
+      authPage.email().type(randomEmail);
+      authPage.password().type('12345678');
+      authPage.signInButton().click();
     });
     it('GA-25 Login invalid data - password', ()=>{
       cy.get('.nav-link').contains('Login').click();
-      cy.get('#email').type('test@test.com');
-      cy.get('#password').type(randomPassword);
-      cy.get('[type=submit]').click();
+      authPage.email().type('test@test.com');
+      authPage.password().type(randomPassword);
+      authPage.signInButton().click();
     });
   });
-describe.only('Logged', ()=>{
-  it('GA-32 : User is logged', ()=> {
+describe('Logged', ()=>{
+  beforeEach('Should visit Login page', ()=>{
     cy.visit('/');
+    cy.server();
+    cy.route(Cypress.env('apiUrl') + '/galleries?page=1&term=').as('galleries');
+  });
+  it('GA-32 : User is logged', ()=> {
     cy.get('.nav-link').contains('Login').click()
-    cy.get('#email').type('jelllenakrstic@gmail.com')
-    cy.get('#password').type('jelenak1908')
-    cy.get('[type=submit]').click()
-    cy.wait(1000)
-    cy.get('.nav-link').contains('Logout').should('be.visible')
-    cy.get('.nav-link').contains('My Galleries')
-    cy.get('.nav-link').contains('Create Gallery')
-    cy.get('.nav-link').contains('Logout')
-    cy.get('.title-style').contains('All Galleries')
-    cy.wait(3000);
+    authPage.email().type('jelllenakrstic@gmail.com');
+    authPage.password().type('jelenak1908');
+    authPage.signInButton().click();
+    cy.wait('@galleries');
+    cy.get('.nav-link').contains('Logout').should('be.visible');
+    cy.get('.nav-link').contains('My Galleries');
+    cy.get('.nav-link').contains('Create Gallery');
     cy.get('.form-control').should('have.attr', 'placeholder', 'Search...');
     cy.get('[type=button]').should('be.visible');
   });
