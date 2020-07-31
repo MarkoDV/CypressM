35be38bbb556b15aa378bf54c37a1035d969962b
MarkoDV <macanes02@gmail.com>
2020-07-31 12:23:59 +0200

    CyG

--- cypress/integration/createGall.js
+++ cypress/integration/createGall.js
@@ -12,14 +12,16 @@ describe('Create gallery', ()=>{
     beforeEach('Visit login page and login', ()=>{
         cy.visit('/login');
         cy.server();
+        cy.route(Cypress.env('apiUrl')).as('hp');
         cy.route(Cypress.env('apiUrl') + '/galleries?page=1&term=').as('galleries');
+        cy.route(Cypress.env('apiUrl') + '/create').as('createGall');
         cy.get('.nav-link').contains('Login').click();
         authPage.login(user.email, user.password);
         cy.wait('@galleries');
     });
     it('GA-12 : Create New Gallery Page validation',()=>{
         createGall.creation(randomTitle, randomDescription, 'https://media-cdn.tripadvisor.com/media/photo-p/10/03/28/52/pogled-iz-baste-na-zalazak.jpg');
-
+        cy.url().should("eq", "https://gallery-app.vivifyideas.com/");
     });
     it('GA-33 : Home Page - paginacija Logged in user 20 galleries',()=>{
         cy.get('.nav-link').contains('My Galleries').should('be.visible').click();
@@ -28,10 +30,12 @@ describe('Create gallery', ()=>{
             createGall.creation(randomTitle, randomDescription, 'https://media-cdn.tripadvisor.com/media/photo-p/10/03/28/52/pogled-iz-baste-na-zalazak.jpg');
             cy.url().should('eq','https://gallery-app.vivifyideas.com/');
         };
+        cy.get('div.grid').children().should('have.length', 10);
     });
     it('Load more button', ()=>{
         cy.get('.nav-link').contains('My Galleries').should('be.visible').click();
         cy.url().should('eq', 'https://gallery-app.vivifyideas.com/my-galleries');
+        cy.scrollTo('bottom');
         cy.get(':nth-child(3) > :nth-child(2) > .btn').should('not.exist');
     });
     it('Delete My Galleries', ()=>{
@@ -39,6 +43,10 @@ describe('Create gallery', ()=>{
            cy.visit('/my-galleries');
            cy.get('.box-title').eq(0).click();
            cy.get('.btn-custom').contains('Delete Gallery').should('be.visible').click(); 
-       }; 
+       }
+    }); 
+    it('No gallery found', ()=>{
+        cy.get('.nav-link').contains('My Galleries').click();
+        cy.get('h4').contains("No galleries found").should('be.visible');
     });
-});
+  });
